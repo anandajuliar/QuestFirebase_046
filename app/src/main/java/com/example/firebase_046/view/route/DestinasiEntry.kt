4e2f7ee5fbee5caaacb2a52fb7c8386c4e393201
anandajuliar <anandajulia001@gmail.com>
2026-01-02 18:13:17 +0700

    menambahkan rute - destinasi entry

---
+++ app/src/main/java/com/example/firebase_046/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.firebase_046.view.route
+
+import com.example.firebase_046.view.route.DestinasiNavigasi
+import com.example.firebase_046.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
