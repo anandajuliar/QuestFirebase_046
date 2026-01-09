d79ce49f0d1853d300ba9be2c4053d9943e0e2f1
anandajuliar <anandajulia001@gmail.com>
2026-01-09 14:56:53 +0700

    add siswa topn app bar at edit page

--- app/src/main/java/com/example/firebase_046/view/HalamanEdit.kt
+++ app/src/main/java/com/example/firebase_046/view/HalamanEdit.kt
@@ -1,2 +1,23 @@
 package com.example.firebase_046.view
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEdit.titleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
