1e1ad35b9fceecb8ca05f43eee14c6162e4681be
anandajuliar <anandajulia001@gmail.com>
2026-01-09 14:35:01 +0700

    feat(di): register DetailViewModel with SavedStateHandle support

--- app/src/main/java/com/example/firebase_046/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/firebase_046/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.firebase_046.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -13,5 +14,12 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
         initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer {
+            DetailViewModel(
+                createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+
     }
 }
