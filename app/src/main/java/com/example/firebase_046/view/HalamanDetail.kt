77640f63fd7478280180ce00fc56207c2030b94d
anandajuliar <anandajulia001@gmail.com>
2026-01-09 15:12:46 +0700

    layar utama halaman edit

--- app/src/main/java/com/example/firebase_046/view/HalamanDetail.kt
+++ app/src/main/java/com/example/firebase_046/view/HalamanDetail.kt
@@ -1,2 +1,64 @@
 package com.example.firebase_046.view
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.firebase_046.R
+import com.example.firebase_046.view.route.DestinasiDetail
+import com.example.firebase_046.viewmodel.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailSiswaScreen(
+    navigateToEditItem: (String) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiDetail.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    val state = viewModel.detailUiState
+                    if (state is DetailUiState.Success) {
+                        navigateToEditItem(state.siswa.id.toString())
+                    }
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.edit_siswa)
+                )
+            }
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        BodyDetailSiswa(
+            detailUiState = viewModel.detailUiState,
+            retryAction = { viewModel.getSiswaById() },
+            onDeleteClick = {
+                viewModel.deleteSiswa()
+                navigateBack()
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
