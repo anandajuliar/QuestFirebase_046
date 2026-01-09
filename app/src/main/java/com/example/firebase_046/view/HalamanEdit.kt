c2dc9665be3be70f9db35cf9250619ebc473db68
anandajuliar <anandajulia001@gmail.com>
2026-01-09 14:58:28 +0700

    updating edit page

--- app/src/main/java/com/example/firebase_046/view/HalamanEdit.kt
+++ app/src/main/java/com/example/firebase_046/view/HalamanEdit.kt
@@ -1,8 +1,15 @@
 package com.example.firebase_046.view
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.firebase_046.view.route.DestinasiEdit
+import com.example.firebase_046.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -21,3 +28,18 @@ fun EditSiswaScreen(
             )
         },
         modifier = modifier
+    ) { innerPadding ->
+        val coroutineScope = rememberCoroutineScope()
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.editSatuSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
